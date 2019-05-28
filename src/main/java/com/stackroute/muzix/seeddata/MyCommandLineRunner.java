9ba8b6b2c6eb80bfbaff26f409b8f8915c655701
khushbu <khushbuk316@gmail.com>
2019-05-28 10:05:33 +0530

    modified repository test

--- src/main/java/com/stackroute/muzix/seeddata/MyCommandLineRunner.java
+++ src/main/java/com/stackroute/muzix/seeddata/MyCommandLineRunner.java
@@ -1,32 +1,32 @@
-package com.stackroute.muzix.seeddata;
-
-import com.stackroute.muzix.domain.Track;
-import com.stackroute.muzix.service.TrackService;
-import lombok.Data;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
-import org.springframework.core.env.Environment;
-import org.springframework.stereotype.Component;
-
-@Component
-@Data
-public class MyCommandLineRunner implements CommandLineRunner {
-    @Autowired
-    TrackService trackService;
-
-    Track track=new Track();
-
-    @Autowired
-    Environment env;
-
-    @Override
-    public void run(String... args) throws Exception {
-        track.setTrackId(Integer.parseInt(env.getProperty("song-id")));
-        track.setTrackName(env.getProperty("song-name"));
-        track.setComment(env.getProperty("song-comment"));
-
-        System.out.println(trackService.saveTrack(track));
-
-
-    }
-}
+//package com.stackroute.muzix.seeddata;
+//
+//import com.stackroute.muzix.domain.Track;
+//import com.stackroute.muzix.service.TrackService;
+//import lombok.Data;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.CommandLineRunner;
+//import org.springframework.core.env.Environment;
+//import org.springframework.stereotype.Component;
+//
+//@Component
+//@Data
+//public class MyCommandLineRunner implements CommandLineRunner {
+//    @Autowired
+//    TrackService trackService;
+//
+//    Track track=new Track();
+//
+//    @Autowired
+//    Environment env;
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        track.setTrackId(Integer.parseInt(env.getProperty("song-id")));
+//        track.setTrackName(env.getProperty("song-name"));
+//        track.setComment(env.getProperty("song-comment"));
+//
+//        System.out.println(trackService.saveTrack(track));
+//
+//
+//    }
+//}
--- src/test/java/com/stackroute/muzix/repository/TrackRepositoryTest.java
+++ src/test/java/com/stackroute/muzix/repository/TrackRepositoryTest.java
@@ -7,6 +7,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -16,14 +17,12 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-
+@AutoConfigureTestDatabase(replace=AutoConfigureTestDatabase.Replace.NONE)
 public class TrackRepositoryTest
 
 {
 
-
-
-        @Autowired
+    @Autowired
         private TrackRepository trackRepository;
         private Track track;
 
